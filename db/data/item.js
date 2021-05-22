8da6454ae6a932d2ff11b58b082f93af7277dd07
Abubakar Siddique <abz.siddique@gmail.com>
2021-05-22 11:32:51 +0100

    feat: Add initial items data

---
+++ db/data/item.js
@@ -0,0 +1,102 @@
+export default [
+  {
+    name: 'tomato',
+    category: 'vegetable',
+    icon: '🍅',
+  },
+  {
+    name: 'bread',
+    category: 'carbohydrate',
+    icon: '🍞',
+  },
+  {
+    name: 'milk',
+    category: 'dairy',
+    icon: '🥛',
+  },
+  {
+    name: 'egg',
+    category: 'protein',
+    icon: '🍳',
+  },
+  {
+    name: 'butter',
+    category: 'fat',
+    icon: '🧈',
+  },
+  {
+    name: 'lettuce',
+    category: 'vegetable',
+    icon: '🥬',
+  },
+  {
+    name: 'pasta',
+    category: 'carbohydrate',
+    icon: '🍝',
+  },
+  {
+    name: 'cheese',
+    category: 'dairy',
+    icon: '🧀',
+  },
+  {
+    name: 'chicken',
+    category: 'protein',
+    icon: '🐓',
+  },
+  {
+    name: 'sunflower oil',
+    category: 'fat',
+    icon: '🌻',
+  },
+  {
+    name: 'onion',
+    category: 'vegetable',
+    icon: '🧅',
+  },
+  {
+    name: 'potato',
+    category: 'carbohydrate',
+    icon: '🥔',
+  },
+  {
+    name: 'plain yoghurt',
+    category: 'dairy',
+    icon: '🍦',
+  },
+  {
+    name: 'tuna',
+    category: 'protein',
+    icon: '🍣',
+  },
+  {
+    name: 'red chilli',
+    category: 'vegetable',
+    icon: '🌶',
+  },
+  {
+    name: 'rice',
+    category: 'carbohydrate',
+    icon: '🍚',
+  },
+  {
+    name: 'plain nuts',
+    category: 'protein',
+    icon: '🥜',
+  },
+  {
+    name: 'green pepper',
+    category: 'vegetable',
+    icon: '🫑',
+  },
+  {
+    name: 'bagel',
+    category: 'carbohydrate',
+    icon: '🥯',
+  },
+  {
+    name: 'beef',
+    category: 'protein',
+    icon: '🥩',
+  }
+]
