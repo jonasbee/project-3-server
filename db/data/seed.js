e6f733866abab8cd23f62eead04246437ea8a212
Dimitar <dconev@gmail.com>
2021-05-23 10:17:22 +0100

    great new feature

---
+++ db/data/seed.js
@@ -0,0 +1,27 @@
+// import Item from '. ./models/item.js ???'
+import connectToDb from './connectToDb.js'
+import mongoose from 'mongoose'
+import itemData from './item.js'
+
+
+async function seedDatabase() {
+  try {
+    await connectToDb()
+    console.log('✅ Connected to mongo')
+
+    await mongoose.connection.db.dropDatabase()
+    console.log('removed all items')
+  
+    const item = await item.create(itemData)
+    console.log(`🤖${item.length} items created`)
+
+    await mongoose.connection.close()
+    console.log('Disconnected from mongo.All done ')
+  } catch (e)   {
+    console.log('Someting when wrong')
+    console.log(e)
+    await mongoose.connection.close()
+  }
+}
+
+seedDatabase()
