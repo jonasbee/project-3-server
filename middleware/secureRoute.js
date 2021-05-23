0938bb95bb90de51be9a2091ce4ebecfd89efba1
Abubakar Siddique <abz.siddique@gmail.com>
2021-05-23 16:34:11 +0100

    feat: Add secure route to post request

--- middleware/secureRoute.js
+++ middleware/secureRoute.js
@@ -1,27 +1,27 @@
-// import jwt from 'jsonwebtoken'
-// import User from '../model/users.js'
-// import { secret } from '../config/environment.js'
+import jwt from 'jsonwebtoken'
+import User from '../model/user.js'
+import { secret } from '../config/environment.js'
 
-// export default function secureRoute(req, res, next) {
+export default function secureRoute(req, res, next) {
 
-//   const rawToken = req.headers.authorization
-//   if (!rawToken || !rawToken.startsWith('Bearer')) {
-//     return res.status(401).json({ message: 'Unauthorized ' })
-//   }
+  const rawToken = req.headers.authorization
+  if (!rawToken || !rawToken.startsWith('Bearer')) {
+    return res.status(401).json({ message: 'Unauthorized ' })
+  }
 
-//   const token = rawToken.replace('Bearer ', '')
-//   console.log(token)
+  const token = rawToken.replace('Bearer ', '')
+  console.log(token)
 
 
-//   jwt.verify(token, secret, async (err, payload) => {
-//     if (err) {
-//       return res.status(401).json({ message: 'Unauthorized ' })
-//     }
-//     const user = User.findById(payload.userId)
-//     if (!user) {
-//       return res.status(401).json({ message: 'Unauthorized ' })
-//     }
-//     req.currentUser = user
-//     next()
-//   })
-// }
+  jwt.verify(token, secret, async (err, payload) => {
+    if (err) {
+      return res.status(401).json({ message: 'Unauthorized ' })
+    }
+    const user = User.findById(payload.userId)
+    if (!user) {
+      return res.status(401).json({ message: 'Unauthorized ' })
+    }
+    req.currentUser = user
+    next()
+  })
+}
