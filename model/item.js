d3173585e475eb8d1181b64d7e18b5b3a5a1122c
Abubakar Siddique <abz.siddique@gmail.com>
2021-05-23 15:10:46 +0100

    feat: Restructure item

--- model/item.js
+++ model/item.js
@@ -3,10 +3,9 @@ import mongoose from 'mongoose'
 
 const itemSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  category: { type: String, required: true },
   icon: { type: String, required: true },
-  quantity: { type: Number, required: false },
-  expiryDate: { type: Date, required: false },
+  quantity: { type: Number, required: true },
+  expiryDate: { type: Date, required: true },
   // ? Referenced relationship. Each item is linked to a particular user.
   // ? The type is a unique Object in MongoDB
   // ? Ref - Use the User schema
