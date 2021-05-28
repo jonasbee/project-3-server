ff38185a8c519106b324d348d4a1fc53e579e343
jonasbolduan@posteo.de <jonasbolduan@posteo.de>
2021-05-28 12:15:00 +0200

    adjustments

--- controllers/user.js
+++ controllers/user.js
@@ -6,7 +6,7 @@ import { getCoordinates } from '../lib/api.js'
 
 async function register(req, res, next) {
   try {
-    
+
     // ? get coordinates of entered address
     // ? via API
     // ? add coordinates to body before creating user
@@ -15,16 +15,23 @@ async function register(req, res, next) {
     // const address = req.body.addressDetails
     // console.log('address:', address)
 
-    const { data } = await getCoordinates(req.body.postalCode, req.body.city, req.body.street, req.body.streetNo, req.body.region, req.body.country)
+    console.log(req.body.postalCode, req.body.city, req.body.street, req.body.streetNo, req.body.region, req.body.country)
 
-    console.log('geocodeArray:', data)
-    const latitude = data[0].lat
-    console.log('latitude:', latitude)
-    const longitude = data[0].lon
-    console.log('longitude:', longitude)
-    console.log(req.body)
-    req.body.coordinates[0] = latitude
-    req.body.coordinates[1] = longitude
+    try {
+      const { data } = await getCoordinates(req.body.postalCode, req.body.city, req.body.street, req.body.streetNo, req.body.region, req.body.country)
+
+      console.log('geocodeArray:', data)
+      const latitude = data[0].lat
+      console.log('latitude:', latitude)
+      const longitude = data[0].lon
+      console.log('longitude:', longitude)
+      console.log(req.body)
+      req.body.coordinates[0] = latitude
+      req.body.coordinates[1] = longitude
+    } catch (error) {
+      console.log(error)
+    }
+
 
     const user = await User.create(req.body)
     console.log(req.body)
@@ -34,11 +41,11 @@ async function register(req, res, next) {
   }
 }
 
-async function login(req,res,next) {
+async function login(req, res, next) {
   try {
     const user = await User.findOne({ email: req.body.email })
-    
- 
+
+
     if (!user) {
       throw new NotValid('There was a problem logging in.')
     }
@@ -52,7 +59,7 @@ async function login(req,res,next) {
       secret,
       { expiresIn: '72h' }
     )
-    
+
     console.log('Success!')
     res.status(202).json({ message: 'Login successful', token })
 
