f8100858f75c986db5c8847861e27196082dfe70
Dimitar <dconev@gmail.com>
2021-05-31 14:40:01 +0100

    adding recipes'

--- db/data/recipes.js
+++ db/data/recipes.js
@@ -5,8 +5,12 @@ export default [
     serves: '4',
     instructionSteps: [
       'Cook the fusilli to pack instructions. Drain well and set aside to cool slightly.',
-      'Meanwhile, lower the eggs into a separate pan of boiling water. Boil for 10 mins, then drain and rinse under cold water to cool. Remove the shells and halve or quarter.',
-      'Whisk the oil with the vinegar in a large bowl; season. Stir through the onion, spinach, tomatoes, cucumber and olives. Add the pasta and toss everything together to coat well in the dressing. Fold in the tuna and most of the parsley, then transfer to plates or a serving platter. Top with the eggs, remaining parsley and extra black pepper to serve.'
+      'Meanwhile, lower the eggs into a separate pan of boiling water. Boil for 10 mins, then drain and rinse under cold water to cool.',
+      'Remove the shells and halve or quarter.',
+      'Whisk the oil with the vinegar in a large bowl; season. Stir through the onion, spinach, tomatoes, cucumber and olives.',
+      'Add the pasta and toss everything together to coat well in the dressing.' ,
+      'Fold in the tuna and most of the parsley, then transfer to plates or a serving platter.',
+      'Top with the eggs, remaining parsley and extra black pepper to serve.'
     ],
     difficulty: 'Easy',
     image: 'https://res.cloudinary.com/djxh1yrnv/image/upload/v1622394713/nicoise-pasta-salad_bxsf99.jpg',
@@ -65,6 +69,157 @@ export default [
       { name: 'banana', number: '2' }
     ],
     preference: 'vegetarian',
+  },
+
+  {
+    name: 'Oat cookies',
+    timeRequired: '25 mins',
+    serves: '12',
+    instructionSteps: [
+      'Preheat the oven to 170C/150C Fan/Gas 3 and line two baking trays with baking paper.' ,
+      'Briefly whizz the oats in a food processor until they are chopped to half their original size. Add the bicarbonate of soda and whizz for another 5 seconds then tip into a mixing bowl.',
+      'Add the peanut butter, sugar and egg (and raisins if using) and mix until thoroughly combined',
+      'Roll the mixture into walnut-sized balls and arrange on the trays, leaving a little space between each one. Flatten each cookie to about 1cm/½in thick.',
+      'Serve chilled, or put it into a pot and keep as a lunchbox treat.',
+      'Bake for 12–14 minutes until crisp and starting to turn golden brown. Leave the cookies to cool on the baking tray to firm up.'
+    ],
+    difficulty: 'Easy',
+    image: 'https://res.cloudinary.com/d-tsonev/image/upload/v1622412415/oat_cookies_10763_16x9_ojvfhi.jpg',
+    ingredients: [
+      { name: 'oats', number: '125', metric: 'g' },
+      { name: 'bicarbonate of soda', number: '10', metric: 'g' },
+      { name: 'peanut (or other nut) butter', number: '125', metric: 'g' },
+      { name: 'caster sugar', number: '125', metric: 'g' },
+      { name: 'egg', number: '1' },
+      { name: 'raisins ', number: '50', metric: 'g' }
+    ],
+    preference: 'vegetarian',
+  },
+
+  {
+    name: 'Moroccan-style roasted veg',
+    timeRequired: '45 mins',
+    serves: '2',
+    instructionSteps: [
+      'Preheat the oven to 220C/200C Fan/Gas 7.',
+      'Put the peppers, sweet potatoes, red onions, whole cherry tomatoes, harissa spice mix, oil and a generous amount of salt and pepper on a large baking sheet.', 
+      'Toss well.',
+      'Roast for 30 minutes, or until everything is soft and charred, tossing the tray halfway through.'
+    ],
+    difficulty: 'Easy',
+    image: 'https://res.cloudinary.com/d-tsonev/image/upload/v1622413247/5-ingredient_33925_16x9_mistoi.jpg',
+    ingredients: [
+      { name: 'peppers', number: '2', metric: 'tsp' },
+      { name: 'sweet potatoes', number: '2' },
+      { name: 'cherry tomatoes', number: '200',metric: 'g' },
+      { name: 'olive oil', number: '1' ,metric: 'tsp' },
+      { name: 'harissa', number: '1',metric: 'tsp' }
+    ],
+    preference: 'vegetarian',
+  },
+
+  {
+    name: 'Feta and pomegranate salad',
+    timeRequired: '25 mins',
+    serves: '4',
+    instructionSteps: [
+      'Put the feta in a shallow dish, squeeze over the lemon juice and scatter over the garlic.',
+      'Leave to marinate while you prepare the rest of the salad.',
+      'Toast the almonds under a medium grill until lightly browned, then leave to cool.',
+      'Cut the pomegranate in half and pop the seeds out into a bowl by bashing the skin with a spoon',
+      'Toss together the rocket, spinach and watercress in a large salad bowl with the olive oil, and salt and freshly ground pepper.', 
+      'Crumble in the marinated feta, and scatter the pomegranate seeds and toasted almonds over. Serve immediately.'
+    ],
+    difficulty: 'Easy',
+    image: 'https://res.cloudinary.com/d-tsonev/image/upload/v1622414010/leafysaladwithfetaan_83561_16x9_bicrrj.jpg',
+    ingredients: [
+      { name: 'feta', number: '200', metric: 'g' },
+      { name: 'garlic gloves', number: '2' },
+      { name: 'almont flakes', number: '100',metric: 'g' },
+      { name: 'mix rocket', number: '120' ,metric: 'g' },
+      { name: 'olive oil', number: '3',metric: 'tbsp' },
+      { name: 'pomegranate', number: '2', metric: 'seeds only' }
+    ],
+    preference: 'vegetarian',
+  },
+
+  {
+    name: 'Spaghetti aglio e olio',
+    timeRequired: '20 mins',
+    serves: '4',
+    instructionSteps: [
+      'Bring a pot of salted water to the boil and add the spaghetti. Cook until the spaghetti is soft but still has a little bite.',
+      'While the spaghetti is cooking, remove the seeds from the chillies and finely chop. Peel and finely chop the garlic.',
+      'Heat the olive oil in a large frying pan. Cook the garlic and chillies gently for a few minutes.',
+      'Drain the spaghetti and add to the frying pan, mix to coat with the oil.', 
+      'Sprinkle the spaghetti with the parsley, season with salt and serve.'
+    ],
+    difficulty: 'Easy',
+    image: 'https://res.cloudinary.com/d-tsonev/image/upload/v1622414507/spaghetti_with_chilli_89628_16x9_vwyyuh.jpg',
+    ingredients: [
+      { name: 'pasta', number: '400', metric: 'g' },
+      { name: 'garlic gloves', number: '3' },
+      { name: 'chilly', number: '3' },
+      { name: 'parsley', number: '1' ,metric: 'tbsp' },
+      { name: 'olive oil', number: '5',metric: 'tbsp' }
+    ],
+    preference: 'vegetarian',
+  },
+
+  {
+    name: '4-ingredient soups',
+    timeRequired: '45 mins',
+    serves: '4',
+    instructionSteps: [
+      'Heat the oil in a large non-stick saucepan and gently fry the onions for 6–8 minutes, or until softened and beginning to brown, stirring regularly.',
+      'Add the vegetable chunks and cook for 5 minutes more, stirring occasionally. Stir in the garlic and cook for a few seconds more.',
+      'Crumble over the stock cubes, add 1 litre/1¾ pints water and bring to the boil.',
+      'Reduce the heat to a simmer, cover the pan loosely with a lid and cook for about 20 minutes or until the vegetables are very soft, stirring occasionally.',
+      'Blitz the soup with a stick blender until very smooth and season with plenty of ground black pepper.',
+      'Add a splash of just-boiled water if the soup is a little thick', 
+      'Ladle into bowls, top with freshly chopped parsley, if using, and serve with the bread.'
+    ],
+    difficulty: 'Easy',
+    image: 'https://res.cloudinary.com/d-tsonev/image/upload/v1622467051/root_vegetable_soup_14910_16x9_ho2ouo.jpg',
+    ingredients: [
+      { name: 'onions', number: '2' },
+      { name: 'garlic gloves', number: '2' },
+      { name: ' root vegetables,any kind ,peeled and cut into roughly 2cm/¾in chunks', number: '800',metric: 'g' },
+      { name: 'chicken or vegitable stock', number: '2' ,metric: 'cubes' },
+      { name: ' oil', number: '2',metric: 'tbsp' },
+      { name: 'frechly chopped parsley' }
+    ],
+    preference: 'vegetarian',
+  },
+  {
+    name: 'Salmon and broccoli pasta',
+    timeRequired: '25 mins',
+    serves: '2',
+    instructionSteps: [
+      'Half fill a large pan with salted water and bring to the boil.',
+      'Add the pasta and cook for 7–12 minutes, or according to the packet instructions until just tender, stirring occasionally',
+      'Three minutes before the end of the pasta cooking time, add the broccoli to the water and cook with the pasta for 3 minutes.',
+      'Meanwhile, heat the oil in a medium non-stick frying pan and cook the spring onions, or sliced onion, for 1–3 minutes, or until softened, stirring regularly.', 
+      'Add the salmon pieces and chilli, if using, and cook for about a minute, turning the salmon 3–4 times.',
+      'Add the cream and a ladleful of the pasta cooking water (around 100ml/3½fl oz). Season generously with salt and pepper.',
+      'Bring to a gentle simmer and cook the salmon for 3–4 minutes, turning occasionally (add an extra splash of water if the sauce thickens too much.)',
+      'Drain the pasta and broccoli and return to the pan. Add the creamy salmon sauce and lemon zest and toss together lightly.'
+    ],
+    difficulty: 'Easy',
+    image: 'https://res.cloudinary.com/d-tsonev/image/upload/v1622467625/salmon_broccoli_pasta_39926_16x9_bdm16d.jpg',
+    ingredients: [
+      { name: 'pasta', number: '150' ,metric: 'g' },
+      { name: 'broccoli', number: '150' ,metric: 'g' },
+      { name: 'spring onions', number: '4' },
+      { name: 'salmon', number: '240' ,metric: 'g' },
+      { name: ' oil', number: '1',metric: 'tbsp' },
+      { name: 'chilli', number: '1' ,metric: 'pinch' },
+      { name: 'double cream', number: '100' ,metric: 'g' },
+      { name: 'lemon', number: '0.5' }
+    ],
+    preference: 'vegetarian',
   }
+
+
 ]
 
